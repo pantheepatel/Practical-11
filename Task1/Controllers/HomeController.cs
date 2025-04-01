436e717c81fa923d51385f3d1338834309412832
PantheePatel <patel.panthee.18@gmail.com>
2025-04-02 00:34:45 +0530

    task1 completion

--- Task1/Controllers/HomeController.cs
+++ Task1/Controllers/HomeController.cs
@@ -3,28 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Task1.Models;
 
 namespace Task1.Controllers
 {
     public class HomeController : Controller
     {
+        static List<UserModel> users = new List<UserModel>()
+        {
+            new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Name="Panthee",
+                Email="panthee@gmail.com",
+                DateOfBirth=new DateTime(2004,08,18),
+                Address="Address Demo 1",
+                City="Ahmedabad"
+            }
+        };
         public ActionResult Index()
         {
-            return View();
+            return View(users);
         }
 
-        public ActionResult About()
+        public ActionResult Create()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
-
-        public ActionResult Contact()
+        [HttpPost]
+        public ActionResult Create(UserModel newUser)
         {
-            ViewBag.Message = "Your contact page.";
+            newUser.Id = Guid.NewGuid();
+            users.Add(newUser);
+            return RedirectToAction("Index");
+        }
 
-            return View();
+        public ActionResult View(Guid id)
+        {
+            return View(users.FirstOrDefault(x => x.Id == id));
+        }
+
+        public ActionResult Edit(Guid id)
+        {
+            UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
+            return View(selectedUser);
+        }
+        [HttpPost]
+        public ActionResult Edit(Guid id, UserModel updatedUser)
+        {
+            UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
+            if (selectedUser != null)
+            {
+                // Update existing user properties instead of replacing the object reference
+                selectedUser.Name = updatedUser.Name;
+                selectedUser.Email = updatedUser.Email;
+                selectedUser.DateOfBirth = updatedUser.DateOfBirth;
+                selectedUser.Address = updatedUser.Address;
+                selectedUser.City = updatedUser.City;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(Guid id)
+        {
+            UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
+            return View(selectedUser);
+        }
+        [HttpPost]
+        public ActionResult DeleteConfirm(Guid id) 
+        {
+            UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
+            users.Remove(selectedUser);
+            return RedirectToAction("Index");
         }
     }
 }
