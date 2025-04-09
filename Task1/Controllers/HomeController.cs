4d0a5968245f4f9aee4b6666d018e8c623eb622a
pantheepatel <patel.panthee.18@gmail.com>
2025-04-09 18:23:40 +0530

    solved the problem of list page refreshing on every request, added comments

--- Task1/Controllers/HomeController.cs
+++ Task1/Controllers/HomeController.cs
@@ -1,10 +1,12 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Task1.Models;
-
+// Task 1 Definition
+//Create an MVC project for the demo of CRUD functionality. The user should save the output to a static list created in the controller. The fields to be used for forms should be Name, Date of birth, Address, etc.
+//There should be each action on the controller for create, update, delete & view
 namespace Task1.Controllers
 {
     public class HomeController : Controller
@@ -19,17 +21,30 @@ namespace Task1.Controllers
                 DateOfBirth=new DateTime(2004,08,18),
                 Address="Address Demo 1",
                 City="Ahmedabad"
+            },
+            new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Name="Nehee",
+                Email="nehee@gmail.com",
+                DateOfBirth=new DateTime(2008,10,31),
+                Address="Address Demo 2",
+                City="Ahmedabad"
             }
         };
+
+        // Entry point for the application, will give the list of users
         public ActionResult Index()
         {
             return View(users);
         }
 
+        // Getting the view of creating a new user
         public ActionResult Create()
         {
             return View();
         }
+        // Creating a new user
         [HttpPost]
         public ActionResult Create(UserModel newUser)
         {
@@ -38,16 +53,19 @@ namespace Task1.Controllers
             return RedirectToAction("Index");
         }
 
+        // To view specific user(by id)
         public ActionResult View(Guid id)
         {
             return View(users.FirstOrDefault(x => x.Id == id));
         }
 
+        // getting the view of editing a user by id
         public ActionResult Edit(Guid id)
         {
             UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
             return View(selectedUser);
         }
+        // Editing the user
         [HttpPost]
         public ActionResult Edit(Guid id, UserModel updatedUser)
         {
@@ -64,11 +82,13 @@ namespace Task1.Controllers
             return RedirectToAction("Index");
         }
 
+        // Getting the view of deleting a user by id
         public ActionResult Delete(Guid id)
         {
             UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
             return View(selectedUser);
         }
+        // Deleting the user with Post because forms supports get/post
         [HttpPost]
         public ActionResult DeleteConfirm(Guid id) 
         {
--- Task2/Controllers/HomeController.cs
+++ Task2/Controllers/HomeController.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.UI.WebControls;
 using Task2.Models;
-
+//Task 2 Definition
+//Create an MVC project for the demo of CRUD functionality using partial views.The user should save the output to a static list created in the controller. The fields to be used for forms should be Name, Date of birth, Address, etc.
+//There should be each action on the controller for create, update, delete & view
+//Use Partial views to create views for create, update & delete
 namespace Task2.Controllers
 {
     public class HomeController : Controller
@@ -19,6 +23,15 @@ namespace Task2.Controllers
                 DateOfBirth=new DateTime(2004,08,18),
                 Address="Address Demo 1",
                 City="Ahmedabad"
+            },
+            new UserModel()
+            {
+                Id = Guid.NewGuid(),
+                Name="Nehee",
+                Email="nehee@gmail.com",
+                DateOfBirth=new DateTime(2008,10,31),
+                Address="Address Demo 2",
+                City="Ahmedabad"
             }
         };
         public ActionResult Index()
@@ -29,12 +42,12 @@ namespace Task2.Controllers
         {
             return PartialView("_List", users);
         }
-        
+
         public ActionResult Create()
         {
             return PartialView("_Create");
         }
-        
+
         [HttpPost]
         public ActionResult Create(UserModel newUser)
         {
@@ -54,7 +67,7 @@ namespace Task2.Controllers
             UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
             return PartialView("_Edit", selectedUser);
         }
-        
+
         [HttpPost]
         public ActionResult Edit(UserModel updatedUser)
         {
@@ -76,12 +89,12 @@ namespace Task2.Controllers
             UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
             return PartialView("_Delete", selectedUser);
         }
-        
+
         [HttpPost]
         public ActionResult DeleteConfirm(Guid id)
         {
             UserModel selectedUser = users.Where(user => user.Id == id).FirstOrDefault();
-            users.Remove(selectedUser); 
+            users.Remove(selectedUser);
             return PartialView("_List", users);
         }
     }
